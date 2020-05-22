7a2485aa217731c3f6c2f60e6e9c872650104be9
Teddy Bradsher <teddybradsher@gmail.com>
2020-05-22 02:49:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " or ""
-    or board[index] == nil
+  if board[index] == " " or board[index] == "" or board[index] == nil
     return false
   else board[index] == "X" or "O"
     return true 
