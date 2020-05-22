64c2fc1f42eff8dc2254effb30244bcc0ffa3630
Teddy Bradsher <teddybradsher@gmail.com>
2020-05-22 02:09:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-  index = ["X", "O"]
+  index = ("X", "O")
 end
   
