6644cbef3325c50129459f404aedee477c9f0294
Teddy Bradsher <teddybradsher@gmail.com>
2020-05-22 02:40:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == " " or ""
-    return false 
+    return false
+  elsif board[index] == "x" or "O"
+    return true 
   end
 end
   
